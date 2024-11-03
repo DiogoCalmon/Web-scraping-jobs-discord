0b3c6d1dd46b90000685819e3037664e33109dcc
DiogoCalmon <diogocalmons@gmail.com>
2024-11-03 08:48:27 -0300

    trying to add cli questions

--- script.js
+++ script.js
@@ -1,15 +1,25 @@
 import puppeteer from 'puppeteer-core';
+import readLine from "readline";
+
+const rl = readLine.createInterface({
+    input: process.stdin,
+    output: process.stdout
+})
 
 async function main() {
+
+    const login = r.
+
     const browser = await puppeteer.launch({
          executablePath: '/usr/bin/brave-browser',
          headless: false
         });
     const page = await browser.newPage();
+    await page.goto('https://discord.com/login');
 
-    await page.goto('https://developer.chrome.com/');
-    await page.setViewport({ width: 1080, height: 1024 });
-
+    const login = readLineSync
+    
+    
     //await browser.close();
 }
 
